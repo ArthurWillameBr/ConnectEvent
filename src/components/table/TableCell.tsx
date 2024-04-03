06ad0d0f5b8de0e82b935d99b04be7c0718fbf96
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-03 18:30:40 -0300

    add tailwind merge

--- src/components/table/TableCell.tsx
+++ src/components/table/TableCell.tsx
@@ -1,7 +1,13 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface TableCellProps extends ComponentProps<"td"> {}
 
 export function TableCell(props: TableCellProps) {
-  return <td {...props} className="px-3 py-4 text-sm text-zinc-300 "></td>;
+  return (
+    <td
+      {...props}
+      className={twMerge("px-3 py-4 text-sm text-zinc-300", props.className)}
+    ></td>
+  );
 }
