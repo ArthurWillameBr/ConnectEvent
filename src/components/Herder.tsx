c60875669a5d22255c1c0c26e5926b470923d6cb
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-02 22:21:56 -0300

    add IconButton component

--- src/components/Herder.tsx
+++ src/components/Herder.tsx
@@ -1,16 +1,13 @@
 import Logo from "../../assets/Logo.svg";
+import { NavLink } from "./NavLink";
 
 export function Herder() {
   return (
     <div className="flex items-center gap-5">
       <img src={Logo} alt="Logo" />
       <nav className="flex items-center gap-5 py-2">
-        <a href="" className="font-medium text-sm text-zinc-300">
-          Eventos
-        </a>
-        <a href="" className="font-medium text-sm">
-          Participantes
-        </a>
+        <NavLink href='/eventos' >Eventos</NavLink>
+        <NavLink href='/participantes' >Participantes</NavLink>
       </nav>
     </div>
   );
